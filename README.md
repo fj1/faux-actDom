0c27599bacaec6bcafa5741649b913b323fb73a4
Frances Jurek <fjurek@pros.com>
2018-06-07 16:29:44 -0500

    bootstrap with create-react-app, then add own implementation of ReactDOM.render and React createElement functions

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# faux-actDom
-rewrite react and reactDOM
+Exercise to build a mock version of Redux
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+class ReactDOM {
+  static render(component, element) {
+    return null;
+  }
+}
+
+class React {
+  static createElement(type,
+    props = {},
+    ...children) {
+    return {
+      props: {
+        ...props,
+      },
+      type
+    }
+  }
+}
+
+ReactDOM.render(<div className="hello">Hello World</div>, document.getElementById('root'));
+registerServiceWorker();
