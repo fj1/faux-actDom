2dd35848d16edb1df7ea1126879ef92292a2e531
Frances Jurek <fjurek@pros.com>
2018-08-12 22:01:23 -0500

    handle custom functional component

--- src/index.js
+++ src/index.js
@@ -4,15 +4,30 @@ import '../src/index.css'
 // 'component' could be a func if using a custom component - need to be able to handle
 // 'component' could be a func if using a class - need to be able to handle  
 
+const CustomComponent = () => <div>yo yo yo</div>;
+
 class ReactDOM {
   static render(component, element) {
     if (typeof component === 'string') {
       element.innerText = component;
 
     } else {
-      const newElement = document.createElement(component.type);
-      const {children, className} = component.props;
-      newElement.className = className ? className : '';
+      let newElement;
+      let children;
+      let className;
+      // handle custom component
+      if (typeof component.type === 'function') {
+        const newComponent = component.type();
+        newElement = document.createElement(newComponent.type);
+        children = newComponent.props.children;
+      } 
+      // handle html element
+      else {
+        newElement = document.createElement(component.type);
+        children = component.props;
+        className = component.props;
+        newElement.className = className ? className : '';
+      }
       // recursively render child elements
       for (const child of children) {
         ReactDOM.render(child, newElement);
@@ -46,14 +61,17 @@ class React {
 // ReactDOM.render(<div className="hello"><span className='child'>Hi kids</span></div>, document.getElementById('root'));
 
 // element with many children
-ReactDOM.render(
-  (
-    <div className="hello">
-      <div>
-        <div className='grandchild'>hello</div>
-      </div>
-      <div>world</div>
-    </div>
-  ), 
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   (
+//     <div className="hello">
+//       <div>
+//         <div className='grandchild'>hello</div>
+//       </div>
+//       <div>world</div>
+//     </div>
+//   ), 
+//   document.getElementById('root')
+// );
+
+// custom component
+ReactDOM.render(<CustomComponent />, document.getElementById('root'));
